8005745f5ab22dff3c9697d7d3bc67bd4cce8537
myselfpoulomi <routhpoulomi@gmail.com>
2025-03-22 20:41:13 +0530

    starting backend

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from 'cors';
+
+const app=express();
+
+app.use(cors())
+app.use(express.json())
+
+
+app.listen(3001,()=>{
+    console.log("Server is running");
+})
